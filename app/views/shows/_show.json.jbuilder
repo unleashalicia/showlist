96862e48c492310805d22369bfe55d64a785a866
Alicia Evans <unleash.alicia@gmail.com>
2019-01-26 09:37:43 -0800

    initial setup

---
+++ app/views/shows/_show.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! show, :id, :name, :description, :duration, :created_at, :updated_at
+json.url show_url(show, format: :json)
---
+++ db/migrate/20190126172001_create_shows.rb
@@ -0,0 +1,11 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.text :description
+      t.string :duration
+
+      t.timestamps
+    end
+  end
+end
